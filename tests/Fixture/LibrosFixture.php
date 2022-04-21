34696d1f77e3d693015ff5953cbb3b495e97337b
Frank <frankperman03@gmail.com>
2022-04-21 12:16:38 -0500

    first commit

---
+++ tests/Fixture/LibrosFixture.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+/**
+ * LibrosFixture
+ */
+class LibrosFixture extends TestFixture
+{
+    /**
+     * Init method
+     *
+     * @return void
+     */
+    public function init(): void
+    {
+        $this->records = [
+            [
+                'id' => 1,
+                'nombre' => 'Lorem ipsum dolor sit amet',
+                'imagen' => 'Lorem ipsum dolor sit amet',
+                'created' => '2022-04-21 17:06:16',
+                'modified' => '2022-04-21 17:06:16',
+            ],
+        ];
+        parent::init();
+    }
+}
