deb894d07f02698ea6fa5a0c749fd7eaa8e93bd7
Frank <frankperman03@gmail.com>
2022-04-28 11:09:56 -0500

    Agregando borrado de imagen

--- src/Controller/LibrosController.php
+++ src/Controller/LibrosController.php
@@ -111,10 +111,19 @@ class LibrosController extends AppController
      * @return \Cake\Http\Response|null|void Redirects to index.
      * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
      */
+
     public function delete($id = null)
     {
         $this->request->allowMethod(['post', 'delete']);
         $libro = $this->Libros->get($id);
+
+        //borrar archivo que hemos agregado
+        if(file_exists(WWW_ROOT.'img/libros/'.$libro['imagen'])){
+            //borra la imagen 
+            unlink(WWW_ROOT.'img/libros/'.$libro['imagen']); 
+
+        }
+
         if ($this->Libros->delete($libro)) {
             $this->Flash->success(__('The libro has been deleted.'));
         } else {
